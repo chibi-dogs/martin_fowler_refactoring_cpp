0219e5a50579ad011aba7360baf262f9660063b3
Basab <basabdatta.gupta@tum.de>
2026-05-15 21:58:19 +0530

    wip

--- main.cpp
+++ main.cpp
@@ -1,18 +1,85 @@
 #include <iostream>
+#include <string>
+#include <json_parser/invoice.h>
+#include <json_parser/play>
+#include <json_parser/json_file_parser.h>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+#include "json_parser/play.h"
+
+using namespace json_parser;
+std::string statement (const invoice::Invoice& invoice, const std::unordered_map<std::string, play::Play>& play )
+{
+    std::string result = std::format("Statement for {}\n", invoice.customer );
+    double total_amount = 0;
+    double volume_credit = 0;
+    for (const auto& perf: invoice.performances)
+    {
+        const auto [name, type] = play.at(perf.play_id);
+        auto amount = 0;
+        switch (type)
+        {
+        case play::Type::Tragedy:
+            {
+                amount += 40000;
+                break;
+            }
+        case play::Type::Comedy:  return "comedy";
+        case play::Type::Unknown: return "unknown";
+        }
+    }
+    // let totalAmount = 0;
+    // let volumeCredits = 0;
+    // let result = `Statement for ${invoice.customer}\n`;
+    // const format = new Intl.NumberFormat("enUS",
+    // { style: "currency", currency: "USD",
+    // minimumFractionDigits: 2 }).format;
+    // for (let perf of invoice.performances) {
+    //     const play = plays[perf.playID];
+    //     let thisAmount = 0;
+    //     switch (play.type) {
+    //     case "tragedy":
+    //         thisAmount = 40000;
+    //         if (perf.audience > 30) {
+    //             thisAmount += 1000 * (perf.audience 30)
+    //             ;
+    //             }
+    //         break;
+    //         case "comedy":
+    //         thisAmount = 30000;
+    //         if (perf.audience > 20) {
+    //             thisAmount += 10000 + 500 * (perf.audience 20)
+    //             ;
+    //             }
+    //         thisAmount += 300 * perf.audience;
+    //         break;
+    //         default:
+    //         throw new Error(`unknown type: ${play.type}`);
+    //         }
+    //     // add volume credits
+    //     volumeCredits += Math.max(perf.audience 30,
+    //     0);
+    //     // add extra credit for every ten comedy attendees
+    //     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    //     // print line for this order
+    //     result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\totalAmount += thisAmount;
+    //     }
+    // result += `Amount owed is ${format(totalAmount/100)}\n`;
+    // result += `You earned ${volumeCredits} credits\n`;
+
+    return result;
+
+}
+
 int main()
 {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
     auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
 
     for (int i = 1; i <= 5; i++)
     {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         std::cout << "i = " << i << std::endl;
     }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
+
